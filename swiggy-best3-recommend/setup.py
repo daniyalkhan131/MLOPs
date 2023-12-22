920a589e0678b4f77414445e9325649833a8ae26
Daniyal Khan <daniyalkhan@Daniyals-MacBook-Pro.local>
2023-12-23 04:46:46 +0530

    added mlops reducibility and swiggy project template based on cookiecutter

---
+++ swiggy-best3-recommend/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='building a food recommendation system on the home page according to customer preferences',
+    author='Daniyal Khan',
+    license='',
+)
